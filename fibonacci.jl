d36e2a17f809fda12a690d111d4697b920ed058f
Rahul Horé <hore.rahul@gmail.com>
2014-05-12 20:31:28 -0400

    Adds iterative version of Fibonacci

--- fibonacci.jl
+++ fibonacci.jl
@@ -1,3 +1,13 @@
 # Recursive implementation
 
 fib(n::Int) = n < 2 ? 1 : fib(n - 2) + fib(n - 1)
+
+# Iterative implementation
+
+function ifib(n)
+  j, k = 1, 1
+  for l = 3:n
+    j, k = k, j + k
+  end
+  return k
+end
